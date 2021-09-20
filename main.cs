c59262c9390e0be7352ceea3f7ba91988d7809d8
JoaoVitorScuira <jvscuira@gmail.com>
2021-09-20 14:43:42 -0300

    Correção erros e preparação para início de mods.

--- main.cs
+++ main.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 namespace myFinances
@@ -7,35 +6,7 @@ namespace myFinances
     {
         static void Main(string[] args)
         {
-            var listaPessoa = new Pessoa();
-            var pessoa1 = new Pessoa();
-            pessoa1.Cpf = "202020";
-            Console.WriteLine (pessoa1.Cpf);
-            DateTime data = new DateTime();
-            data = DateTime.Now;
-            Console.WriteLine (data);
-            Console.WriteLine ("Teste repositório");
         }   
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-namespace myFinances
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var listaPessoa = new Pessoa();
-            var pessoa1 = new Pessoa();
-            pessoa1.Cpf = "202020";
-            Console.WriteLine (pessoa1.Cpf);
-            DateTime data = new DateTime();
-            data = DateTime.Now;
-            Console.WriteLine (data);
-            Console.WriteLine ("Teste repositório");
-        }   
-    }
-}
->>>>>>> 5cf3ebba7ba99028bcc35a575d23ccc78193e0fc
+
